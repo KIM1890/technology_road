f838ffd8ad0cec9d077d70d9843e37b80f5097b9
KIM1890 <kimliendo1890@gmail.com>
2021-09-03 17:49:02 +0700

    update url

--- technology_road/settings.py
+++ technology_road/settings.py
@@ -13,6 +13,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import django_heroku
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
@@ -27,7 +29,9 @@ DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1', 'technologyroad.herokuapp.com']
 # staticfiles
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# setting django_heroku
 
+django_heroku.settings(locals())
 # Application definition
 
 INSTALLED_APPS = [
