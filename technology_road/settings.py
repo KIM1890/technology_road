0432d1dc1559ca3e102845b1b437d25c12ad865c
KIM1890 <kimliendo1890@gmail.com>
2021-09-03 16:57:41 +0700

    update database

--- technology_road/settings.py
+++ technology_road/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     'stores',
 ]
 MIDDLEWARE = [
@@ -73,10 +74,20 @@ WSGI_APPLICATION = 'technology_road.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dcmfl79k3vrvej',
+        'USER': 'ratbdkfjgzdcvz',
+        'PASSWORD': 'cf8e4f5e2dc4315452d5b5634aeba388e55fb90fc49797e794b8ef305447fbe1',
+        'HOST': 'ec2-54-83-137-206.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
--- technology_road/urls.py
+++ technology_road/urls.py
@@ -1,13 +1,7 @@
-from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
-from django.views.static import serve
-
-from technology_road import settings
 
 urlpatterns = [
     path('', include('stores.urls')),
     path('admin/', admin.site.urls),
-    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
