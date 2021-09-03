418f76c4742ffaac5e76e21c26bf429ecbf4f288
KIM1890 <kimliendo1890@gmail.com>
2021-09-03 17:37:37 +0700

    update url

--- technology_road/urls.py
+++ technology_road/urls.py
@@ -1,7 +1,12 @@
+from django.conf import settings
+from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.staticfiles.views import serve
 from django.urls import path, include
 
 urlpatterns = [
     path('', include('stores.urls')),
     path('admin/', admin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
